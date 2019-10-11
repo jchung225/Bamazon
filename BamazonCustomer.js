410c417995e03407f54a267a312990227ec5f63f
Jim Chung <jchung225@gmail.com>
2019-10-10 22:12:43 -0400

    database created

---
+++ BamazonCustomer.js
@@ -0,0 +1,45 @@
+var prompt = require('prompt');
+var mysql = require('mysql');
+var padText = require('./padTable.js')
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", 
+    password: "", 
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+
+connection.query('SELECT * FROM Products', function(err, res){
+    if(err) throw err;  
+    console.log('Check out our selection...\n');
+  
+    console.log('  ID  |      Product Name      |  Department Name  |   Price  | In Stock');
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+      for(var i = 0; i < res.length; i++){
+
+      var itemID = res[i].ItemID + ''; 
+      itemID = padText("  ID  ", itemID);
+  
+      var productName = res[i].ProductName + ''; 
+      productName = padText("      Product Name      ", productName);
+  
+      var departmentName = res[i].DepartmentName + ''; 
+      departmentName = padText("  Department Name  ", departmentName);
+  
+      var price = '$' + res[i].Price.toFixed(2) + ''; 
+      price = padText("   Price  ", price);
+  
+      var quantity = res[i].StockQuantity + ''; 
+  
+      console.log(itemID + '|' + productName + '|' + departmentName + '|' + price + '|    ' + quantity);
+    }
+    prompt.start();
+  
+    
